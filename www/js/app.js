ae0b33691f521a068d1a32dff1725b2f3dd45be9
jpcamara <johnpcamara@gmail.com>
2015-01-09 17:00:32 -0500

    making project into a slidemenu with examples of famous angular demos

--- www/js/app.js
+++ www/js/app.js
@@ -30,42 +30,35 @@ angular.module('starter', ['ionic', 'starter.controllers', 'famous.angular'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: "/search",
+  .state('app.cuboid', {
+    url: "/cuboid",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: "templates/cuboid.html",
+        controller: 'CuboidCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-    url: "/browse",
+  .state('app.draggable', {
+    url: "/draggable",
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: "templates/draggable.html",
+        controller: "DraggableCtrl"
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
+  .state('app.flipper', {
+    url: "/flipper",
     views: {
       'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
+        templateUrl: "templates/flipper.html",
+        controller: "FlipperCtrl"
       }
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/cuboid');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -33,16 +33,36 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('CuboidCtrl', function($scope, $stateParams, $famous) {
+  var Timer = $famous['famous/utilities/Timer'];
+  var _colors = ["#b58900","#cb4b16","#dc322f","#6c71c4","#268bd2","#2aa198","#859900"];
+  var SPEED = [.03, -.03, .01];
+  //var _rotate = [Math.PI / 4,0,0];
+  var _rotate = [0,0,0];
+  Timer.every(function(){
+    _rotate[0] += SPEED[0];
+    _rotate[1] += SPEED[1];
+    _rotate[2] += SPEED[2];
+  });
+  $scope.getRotate = function(){
+    return _rotate;
+  };
+  $scope.surfaces = _.map(_.range(6), function(i){
+    return {bgColor: _colors[i]};
+  });
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('DraggableCtrl', function ($scope, $famous) {
+  var EventHandler = $famous['famous/core/EventHandler'];
+  $scope.handler = new EventHandler();
+})
+
+.controller('FlipperCtrl', function ($scope, $famous) {
+  var EventHandler = $famous['famous/core/EventHandler'];
+
+  $scope.evt = new EventHandler();
+
+  $scope.flip = function(){
+    $famous.find('#flipper')[0].flip();
+  }
 });
